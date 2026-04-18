5dccc56dcf3eaf43e267fc94a68b8fd5662190d9
athospozo <athospozoo@gmail.com>
2026-04-18 18:37:23 -0300

    adicionando redis a listagem de users

--- src/http/controller/user/show-all.controller.ts
+++ src/http/controller/user/show-all.controller.ts
@@ -1,12 +1,31 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import { UserPresenter } from '@/http/presenter/user-presenter.js'
+import { redis } from '@/lib/redis.js'
 import { makeShowAllUsersUseCase } from '@/use-case/factories/user/make-show-all-use-case.js'
 
 export async function showAll(_request: FastifyRequest, reply: FastifyReply) {
-  const showAllUsersUseCase = makeShowAllUsersUseCase()
-  const usuarios = await showAllUsersUseCase.execute()
-  if (!usuarios) {
-    return reply.status(200).send([])
+  try {
+    const cacheKey = 'users:all'
+
+    const cachedUsers = await redis.get(cacheKey)
+
+    if (cachedUsers) {
+      return reply.status(200).send({ usuários: JSON.parse(cachedUsers) })
+    }
+
+    const showAllUsersUseCase = makeShowAllUsersUseCase()
+    const usuarios = await showAllUsersUseCase.execute()
+
+    if (!usuarios) {
+      return reply.status(200).send([])
+    }
+
+    const usuariosFormatados = UserPresenter.toHTTP(usuarios)
+
+    await redis.set(cacheKey, JSON.stringify(usuariosFormatados), 'EX', 60)
+
+    return reply.status(200).send({ usuarios: usuariosFormatados })
+  } catch (_error) {
+    return reply.status(400).send({ message: 'Erro ao buscar usuários' })
   }
-  return reply.status(200).send(UserPresenter.toHTTP(usuarios))
 }
